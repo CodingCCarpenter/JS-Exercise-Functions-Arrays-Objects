a19f37d4e5edca607700c7cf352277f64d11456b
CodingCCarpenter <christineassists@aol.com>
2019-12-16 10:48:12 -0600

    completed first task

--- index.js
+++ index.js
@@ -39,9 +39,15 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
-}
+function makePersonObject(idArg, nameArg, emailArg) {
+  let personObject = {
+    id: idArg,
+    name: nameArg,
+    email: emailArg
+  }
+  return personObject
+};
+console.log(makePersonObject(5, 'Leia', 'leia@leia.com'));
 
 /**
  * ### Challenge `getName`
@@ -56,8 +62,10 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(arg) {
+  
+  return (`Hello, my name is ` + arg[name])
+ 
 }
 
 /**
@@ -73,8 +81,8 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(name) {
+  
 }
 
 
